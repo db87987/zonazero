261d0e3c91dc87e02b63eb3b89e038846d338c28
babrovka <babrovka@gmail.com>
2012-08-06 15:24:52 +0400

    active admin index customize1

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Client do
+
+
+   index do
+    column :title
+    column :point1
+    column :point2
+    column :point3
+    column :point4
+    column :point5
+  end
+
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Details" do
     f.input :title
@@ -12,5 +23,26 @@ ActiveAdmin.register Client do
   end
   f.buttons
  end
+
+
+
+
+ show do
+      attributes_table do
+        row :title
+        row :point1
+        row :point2
+        row :point3
+        row :point4
+        row :point5
+        row :logo1 do
+          image_tag(client.logo1.url)
+        end
+        row :logo2 do
+          image_tag(client.logo2.url)
+        end
+      end
+      active_admin_comments
+    end
 
 end
