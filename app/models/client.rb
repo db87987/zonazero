e6a19b93d4598eaebe434faeaa8a9269482a9011
babrovka <babrovka@gmail.com>
2012-08-09 16:25:37 +0400

    knopka naverh1

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
   attr_accessible :title, :logo1, :logo2, :point1, :point2, :point3, :point4, :point5
-
+                  
+    
   has_attached_file :logo1, :styles => { :small => "100x67>", 
                     :url  => "/assets/clients/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/clients/:id/:style/:basename.:extension"
