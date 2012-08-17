a502f00a80862b247394fcfab0bb4c437e4a9e4e
babrovka <babrovka@gmail.com>
2012-08-17 19:05:56 +0400

    ck editor fix5

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Zonazero::Application.initialize!
 
-config.assets.precompile += ['ckeditor/init.js']
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -128,4 +128,6 @@ ActiveAdmin.setup do |config|
   #   config.register_javascript 'my_javascript.js'
   config.register_javascript 'ckeditor/init.js'
   
+  config.assets.precompile += ['ckeditor/init.js']
+  
 end
