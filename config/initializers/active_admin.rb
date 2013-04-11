5488f676b4a3cb2bbdb9625b8d2b1599f7e09b64
Viacheslav Bobrov <babrovka@gmail.com>
2013-04-11 12:46:38 +0400

    update fix

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,8 +6,6 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Zonazero"
-
-  config.root_to = 'awards#index'
   
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
