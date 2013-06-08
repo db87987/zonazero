1238ed1f1b14c25a2b9d80084e6004ec2837f2e7
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-08 19:45:55 +0400

    update deploy recipe & delete public/system folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 require 'rvm/capistrano'
 require 'bundler/capistrano'
 
+load 'deploy/assets'
+
 server "5.178.80.26", :web, :app, :db, primary: true
 
 set :user, "user"
@@ -21,3 +23,18 @@ after "deploy:update_code", :copy_database_config
    db_config = "#{shared_path}/database.yml"
    run "cp #{db_config} #{release_path}/config/database.yml"
 end
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
+after "deploy", "deploy:cleanup"
