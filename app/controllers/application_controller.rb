59ab093390d6fe8d22875a50f25a96572a484651
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-14 22:37:01 +0400

    left menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :miniprofiler
+  #before_filter :miniprofiler
   
-  private
-  def miniprofiler
-    if admin_user_signed_in?
-    logger.info 'Profiling'
-    Rack::MiniProfiler.authorize_request
-    # Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
-    end
-  end
+  # private
+  # def miniprofiler
+  #   if admin_user_signed_in?
+  #   logger.info 'Profiling'
+  #   Rack::MiniProfiler.authorize_request
+  #   # Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+  #   end
+  # end
     
 end
